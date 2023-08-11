f0001c0f52bd1d0258f0b64ab9874f4339ca10a7
tysonOliveira <thaisonoliveira@gmail.com>
2023-08-11 17:05:57 -0300

    feat(editor): floating menu button

--- src/app/components/Editor.tsx
+++ src/app/components/Editor.tsx
@@ -39,9 +39,47 @@ export function Editor() {
   return (
     <>
       <EditorContent className="max-w-[700px] mx-auto pt-16 prose prose-invert prose-violet" editor={editor} />
-      {editor &&
-        <FloatingMenu editor={editor}></FloatingMenu>
-      }
+      {editor && (
+        <FloatingMenu
+          editor={editor}
+          className="bg-zinc-700 py-2 px-1 shadow-xl border gap-1 border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex flex-col"
+          shouldShow={({ state }) => {
+            const { $from } = state.selection
+            const currentLineText = $from.nodeBefore?.textContent
+            return currentLineText === '/'
+          }}
+        >
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+
+          >
+            <img
+              src="http://www.notion.so/images/blocks/text/en-US.png"
+              alt="text"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Text</span>
+              <span className="text-xs text-zinc-400">Just start writing with plain text.</span>
+            </div>
+          </button>
+
+          <button
+            className="flex items-center gap-2 p-1 rounded min-w-[280px] hover:bg-zinc-600"
+            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+          >
+            <img
+              src="http://www.notion.so/images/blocks/header.57a7576a.png"
+              alt="heading"
+              className="w-12 border border-zinc-600 rounded"
+            />
+            <div className="flex flex-col text-left">
+              <span className="text-sm">Heading 1</span>
+              <span className="text-xs text-zinc-400">Big section heading.</span>
+            </div>
+          </button>
+        </FloatingMenu>
+      )}
       {editor && (
         <BubbleMenu className="bg-zinc-700 shadow-xl boder border-zinc-600 shadow-black/20 rounded-lg overflow-hidden flex divide-x divide-zinc-600" editor={editor}>
           <BubbleButton>
